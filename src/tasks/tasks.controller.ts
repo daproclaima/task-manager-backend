f5909560c9e99c913441620e05bcfee26479ff29
daproclaima <sn.itpro@gmail.com>
2020-12-20 12:17:15 +0100

    initial commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  //  to inject a service, it is by the constructor
+  // by default when I add private it creates the field as private
+  constructor(private taskService: TasksService) {}
+}
