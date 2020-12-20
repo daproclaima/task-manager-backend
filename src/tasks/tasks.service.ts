44438b65186d0b461dda45c5e24990e156370c9c
daproclaima <sn.itpro@gmail.com>
2020-12-20 12:42:07 +0100

    feat(task): add task model and update service, controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  // declaring the typehint is not mandatory in ts but bring better design
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
