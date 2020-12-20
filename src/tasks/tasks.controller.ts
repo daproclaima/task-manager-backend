8734244eaa6323c1ba31a2f05a2d83694e5a1214
daproclaima <sn.itpro@gmail.com>
2020-12-20 12:32:59 +0100

    feat(tasks): update controller andservice to expose GET tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,4 +6,9 @@ export class TasksController {
   //  to inject a service, it is by the constructor
   // by default when I add private it creates the field as private
   constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
 }
