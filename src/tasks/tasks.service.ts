6f735fa04d3cc4c848c07744a78f3e8736689986
daproclaima <sn.itpro@gmail.com>
2020-12-20 12:52:05 +0100

    feat(tasks service): add createTask method
    return the task. Frontend devs need it, that way they don t need an extra fetch or refresh the application

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
-
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -8,5 +8,19 @@ export class TasksService {
   // declaring the typehint is not mandatory in ts but bring better design
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    // return the task. Frontend devs need it,
+    // that way they don t need an extra fetch or refresh the application
+    return task;
   }
 }
