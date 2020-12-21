74ef3f8d8b07e19cb74b04429c62ef7f0e3c19a2
daproclaima <sn.itpro@gmail.com>
2020-12-21 22:16:04 +0100

    feat(auth): add user signup method

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  //
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    // do not return the user as it conveys password 😂
+    await user.save();
+  }
 }
