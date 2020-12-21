292196c28a60ec8dc5129970ebbcb3df3c86775f
daproclaima <sn.itpro@gmail.com>
2020-12-21 11:23:55 +0100

    refactor(tasks/): remove task model, create task status enum, comment out controller and service
    The goal here is to use the entity in place of model. To not have errors, I facilitate refactor in commenting out methods related to deleted task.model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,7 +11,6 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
@@ -22,46 +21,46 @@ export class TasksController {
   // by default when I add private it creates the field as private
   constructor(private taskService: TasksService) {}
 
-  @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-    // console.log(filterDto);
-    if (Object.keys(filterDto).length) {
-      return this.taskService.getTasksWithFilters(filterDto);
-    }
-    return this.taskService.getAllTasks();
-  }
-
-  @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.taskService.getTaskById(id);
-  }
-
-  // first option: we extract everything
-  // @Post()
-  // createTask(@Body() body) {
-  //   console.log('body', body);
+  // @Get()
+  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+  //   // console.log(filterDto);
+  //   if (Object.keys(filterDto).length) {
+  //     return this.taskService.getTasksWithFilters(filterDto);
+  //   }
+  //   return this.taskService.getAllTasks();
   // }
-
-  // second option, we only extract what we want
-  @Post()
-  // ValidationPipe is smart enough to use the right validator below
-  @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    // console.log('title:', title);
-    // console.log('description:', description);
-    return this.taskService.createTask(createTaskDto);
-  }
-
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    return this.taskService.deleteTask(id);
-  }
-
-  @Patch('/:id/status')
-  updateTask(
-    @Param('id') id: string,
-    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  ): Task {
-    return this.taskService.updateTaskStatus(id, status);
-  }
+  //
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string): Task {
+  //   return this.taskService.getTaskById(id);
+  // }
+  //
+  // // first option: we extract everything
+  // // @Post()
+  // // createTask(@Body() body) {
+  // //   console.log('body', body);
+  // // }
+  //
+  // // second option, we only extract what we want
+  // @Post()
+  // // ValidationPipe is smart enough to use the right validator below
+  // @UsePipes(ValidationPipe)
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   // console.log('title:', title);
+  //   // console.log('description:', description);
+  //   return this.taskService.createTask(createTaskDto);
+  // }
+  //
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //   return this.taskService.deleteTask(id);
+  // }
+  //
+  // @Patch('/:id/status')
+  // updateTask(
+  //   @Param('id') id: string,
+  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  // ): Task {
+  //   return this.taskService.updateTaskStatus(id, status);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,74 +1,70 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
-
-  // declaring the typehint is not mandatory in ts but bring better design
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
-
-  getTaskById(id: string): Task {
-    const found = this.tasks.find((task) => task.id === id);
-
-    if (!found) {
-      //  Nest provides the error handler,
-      //  we return an error not handled by controller,
-      //  but to nest behind the scene (as an http error excep)
-      /*
-        {
-          "statusCode": 404,
-          "message": "Task with id not found"
-        }
-      */
-      throw new NotFoundException(`Task with ${id} not found`);
-    }
-    return found;
-  }
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.getAllTasks();
-    if (status) {
-      tasks = tasks.filter((task) => task.status === status);
-    }
-    if (search) {
-      tasks = tasks.filter(
-        (task) =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-    return tasks;
-  }
-
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-    // return the task. Frontend devs need it,
-    // that way they don t need an extra fetch or refresh the application
-    return task;
-  }
-
-  deleteTask(id: string): void {
-    // todo: read the tasks array twice. refactor
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter((task) => task.id !== found.id);
-  }
-
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    const task = this.getTaskById(id);
-    task.status = status;
-    return task;
-  }
+  // // declaring the typehint is not mandatory in ts but bring better design
+  // getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
+  //
+  // getTaskById(id: string): Task {
+  //   const found = this.tasks.find((task) => task.id === id);
+  //
+  //   if (!found) {
+  //     //  Nest provides the error handler,
+  //     //  we return an error not handled by controller,
+  //     //  but to nest behind the scene (as an http error excep)
+  //     /*
+  //       {
+  //         "statusCode": 404,
+  //         "message": "Task with id not found"
+  //       }
+  //     */
+  //     throw new NotFoundException(`Task with ${id} not found`);
+  //   }
+  //   return found;
+  // }
+  //
+  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+  //   const { status, search } = filterDto;
+  //   let tasks = this.getAllTasks();
+  //   if (status) {
+  //     tasks = tasks.filter((task) => task.status === status);
+  //   }
+  //   if (search) {
+  //     tasks = tasks.filter(
+  //       (task) =>
+  //         task.title.includes(search) || task.description.includes(search),
+  //     );
+  //   }
+  //   return tasks;
+  // }
+  //
+  // createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //
+  //   this.tasks.push(task);
+  //   // return the task. Frontend devs need it,
+  //   // that way they don t need an extra fetch or refresh the application
+  //   return task;
+  // }
+  //
+  // deleteTask(id: string): void {
+  //   // todo: read the tasks array twice. refactor
+  //   const found = this.getTaskById(id);
+  //   this.tasks = this.tasks.filter((task) => task.id !== found.id);
+  // }
+  //
+  // updateTaskStatus(id: string, status: TaskStatus): Task {
+  //   const task = this.getTaskById(id);
+  //   task.status = status;
+  //   return task;
+  // }
 }
