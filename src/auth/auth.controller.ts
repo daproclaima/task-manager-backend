869682cdb154b62d8c327f5165bc79ff6dc4b94c
daproclaima <sn.itpro@gmail.com>
2020-12-22 17:47:08 +0100

    feat: change start:dev script and enable cors policies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,21 +5,23 @@ import {
   Req,
   UseGuards,
   ValidationPipe,
+  Logger,
 } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './get-user-decorator';
 import { User } from './user.entity';
-
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
   signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
+    this.logger.debug(`Signup data: ${authCredentialsDto}`);
     return this.authService.signUp(authCredentialsDto);
   }
 
@@ -27,6 +29,7 @@ export class AuthController {
   signIn(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
+    this.logger.debug(`Signin data: ${authCredentialsDto}`);
     return this.authService.signIn(authCredentialsDto);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
   const logger = new Logger('bootsrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   // select the config folder based on NODE_ENV
   console.log(serverConfig);
 
