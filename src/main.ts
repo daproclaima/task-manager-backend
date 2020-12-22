e8fe1db781ab355cb39ae0a72b1b191954d2231b
daproclaima <sn.itpro@gmail.com>
2020-12-22 13:26:00 +0100

    feat(config/, src/): add config from config files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 async function bootstrap() {
   /*
   in dev env: Log, Error, Warning, Debug, Verbose,
@@ -9,8 +10,14 @@ async function bootstrap() {
   in production env: Log, Error
   */
   const logger = new Logger('bootsrap');
-  const port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  // select the config folder based on NODE_ENV
+  const serverConfig = config.get('server');
+
+  console.log(serverConfig);
+
+  const port = 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
